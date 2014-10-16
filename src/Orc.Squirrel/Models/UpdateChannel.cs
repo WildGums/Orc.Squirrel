b0077296771ab83839e1e385bc236df0888388c7
Geert van Horrik <geert@catenalogic.com>
2014-10-16 22:38:18 +0200

    Improved UpdateChannel model

--- src/Orc.Squirrel/Models/UpdateChannel.cs
+++ src/Orc.Squirrel/Models/UpdateChannel.cs
@@ -7,11 +7,27 @@
 
 namespace Orc.Squirrel
 {
+    using Catel;
+
     /// <summary>
     /// Update channel model.
     /// </summary>
     public class UpdateChannel
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdateChannel"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="defaultUrl">The default URL.</param>
+        public UpdateChannel(string name, string defaultUrl)
+        {
+            Argument.IsNotNull(() => name);
+            Argument.IsNotNull(() => defaultUrl);
+
+            Name = name;
+            DefaultUrl = defaultUrl;
+        }
+
         /// <summary>
         /// Gets the name.
         /// </summary>
